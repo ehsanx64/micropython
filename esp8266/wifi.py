8a0e49e1b9f6c6c4994f9a5622f20278c5898e04
Ehsan Mohammadi <ehsanx64@gmail.com>
2023-10-26 22:28:33 +0330

    esp8266: add access point setup example

--- esp8266/wifi.py
+++ esp8266/wifi.py
@@ -2,19 +2,24 @@
 
 import network
 
-# Create a station interface
-sta = network.WLAN(network.STA_IF)
-
-# Activate the interface
-sta.active(True)
-
 # Scan for access points and print the result
 def scan_aps():
+    # Create a station interface
+    sta = network.WLAN(network.STA_IF)
+    
+    # Activate the interface
+    sta.active(True)
+    
     aps = sta.scan()
     print(aps)
 
-
 def connect_ap():
+    # Create a station interface
+    sta = network.WLAN(network.STA_IF)
+    
+    # Activate the interface
+    sta.active(True)
+
     if not wlan.isconnected():
         print("Connecting ...")
         wlan.connect("SSID", "PSK")
@@ -25,6 +30,11 @@ def connect_ap():
     print("Connected!")
     print("Network config:", wlan.ifconfig())
 
-scan_aps()
-connect_ap()
+def start_ap():
+    ap = network.WLAN(network.AP_IF)
+    ap.active(True)
+    ap.config(essid="ESP1-AP",password="asdfasdf")
 
+#scan_aps()
+#connect_ap()
+start_ap()
